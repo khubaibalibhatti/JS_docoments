4308a2abedf982a8e0b854d7d74c521c6df58c18
Khubaib Ali <141569694+khubaibalibhatti@users.noreply.github.com>
2024-10-15 07:43:05 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,37 @@
+
+"use strict";// treat all js code as newer version
+
+// alert(4 + 4) // we are using nodejs not browser
+
+console.log(3 
+    + 
+    3) // code readbility should be high
+      
+     // Numbers;
+    let length = 15 + "volvo";
+    let weight = 7.5;
+
+    // strings;
+    let firstname = "khubaib";
+
+    //Booleans
+    let isLogedIn = false
+
+    let state;
+    // number => 2 to power 53
+    // bigint
+    //string =""
+    //boolean => true/false
+    // undefined
+    //null => standalone value
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof "khubaib"); // string
+    console.log(typeof null);// object
+    
+    
+
+
