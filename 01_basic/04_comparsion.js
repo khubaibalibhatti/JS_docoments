fd24249a50c4ce45710fc4547443495d21e68661
Khubaib Ali <141569694+khubaibalibhatti@users.noreply.github.com>
2024-10-15 08:42:16 +0000

    comparsion

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,20 @@
+
+// console.log(3 > 1);
+// console.log(3 >= 1);
+// console.log(3 < 1);
+// console.log(3 == 1);
+// console.log(3 != 1);
+
+
+// console.log("4" > 1);
+// console.log("04" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+ 
+
