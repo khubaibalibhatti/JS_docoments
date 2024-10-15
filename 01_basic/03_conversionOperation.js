6aa7e1b92379cbadff83e4b7e0f1d9578dbdf9c2
Khubaib Ali <141569694+khubaibalibhatti@users.noreply.github.com>
2024-10-15 08:06:36 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,36 @@
+
+
+let score = "21abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+
+
+
+//"33" => 33
+// "30abd" => NaN
+// tue => 1; false =>0
+
+
+let isLoggedIn = "44"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+//"" => false
+//"khubaib" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
