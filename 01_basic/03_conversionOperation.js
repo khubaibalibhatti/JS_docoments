9fbc476a16f5e26bbcebe4f8d0d97f6909378d89
Khubaib Ali <141569694+khubaibalibhatti@users.noreply.github.com>
2024-10-15 08:29:39 +0000

    prefix and postfix read

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -2,12 +2,12 @@
 
 let score = "21abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueNumber = Number(score)
-console.log(typeof valueNumber);
+// console.log(typeof valueNumber);
 
 
 
@@ -21,7 +21,7 @@ let isLoggedIn = "44"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -31,6 +31,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 44
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********Operation ***********//
+
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+
+let str1 = "hello"
+let str2 = " khubaib"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 3);
+// console.log(3 + "1");
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "1");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+ 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
 
